11f4169dd0e581f1938d4f502ff4ffd6c50d3ce1
Arie van Luttikhuizen <aluttik@gmail.com>
2015-08-09 00:15:30 +0100

    Use dateutils for get_date()

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -5,44 +5,43 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime, date
+from dateutil.parser import parse
 from json import dumps
 import os
 import re
 
-def get_when_visible(driver, locator, timeout=30):
+# open a new chromedriver window
+path_to_chromedriver = os.environ['PATH_TO_CHROMEDRIVER']
+browser = webdriver.Chrome(executable_path = path_to_chromedriver)
+
+
+def get_element(locator, driver=browser, timeout=30):
     '''Waits until a web element is be visible, then returns it.
 
     Parameters:
-        driver (webdriver) - the webdriver that's being used
+        driver (webdriver) - the webdriver to look in (default=browser)
         locator (locator) - the webelement locator object
-        timeout (int) - seconds to wait before timeout (default 60)
+        timeout (int) - seconds to wait before timeout (default=60)
     Returns:
         The web element found using the locator
     Raises:
         TimeoutException - when no element is found before timeout
     '''
-    return wait(driver, timeout).until(EC.visibility_of_element_located(locator))
+    _is_visible = EC.visibility_of_element_located
+    return wait(driver, timeout).until(_is_visible(locator))
 
 def get_date(top_elem, css_selector):
     '''Finds the date from an HTML element
 
     Parameters:
-        top_elem (webelement) - the element that represents the entire interaction
-        css_selector (str) - css selector pointing to the element with the date
+        top_elem (webelement) - a parent of the date element
+        css_selector (str) - points to the element being read
     Returns:
         string representation of the date in ISO format
     '''
     date_elem = top_elem.find_element_by_css_selector(css_selector)
-    date_search = re.search('([A-Za-z]{3} \d?\d)(?:, )?(\d{4})?', date_elem.text)
-    # if the date was found, return it
-    if date_search:
-        # if the date doesn't specify a year, assume the current year
-        year = (date_search.group(2) if date_search.group(2) else str(date.today().year))
-        return str(datetime.strptime(date_search.group(1) + ' ' + year, '%b %d %Y').date())
-    # if the date wasn't found, check if the text describes a time of day
-    elif re.search('([0-2]?\d:[0-5]\d [apAP][mM])', date_elem.text):
-        # if it does, return today
-        return date.today().isoformat();
+    date_string = date_elem.text.split('-')[0]
+    return parse(date_string, fuzzy=True).date().isoformat()
 
 def get_contacts(top_elem):
     '''Collects all of the contacts found in a "other contacts" popup
@@ -68,26 +67,24 @@ def get_contacts(top_elem):
         contact_list[span.text.encode('utf8')] = 'notMatched'
     return contact_list
 
-# get information from environment variables
-email = os.environ['NIMBLE_EMAIL']
-password = os.environ['NIMBLE_PASSWORD']
-path_to_chromedriver = os.environ['PATH_TO_CHROMEDRIVER']
-
-# open a new chrome web driver and go to nimble.com
-browser = webdriver.Chrome(executable_path = path_to_chromedriver)
+# go to nimble.com
 url = "https://nginx.nimble.com/#app/contacts/view?id=5581fe938e08ab59fe6dd915"
 browser.get(url)
 
+# get information from environment variables
+email = os.environ['NIMBLE_EMAIL']
+password = os.environ['NIMBLE_PASSWORD']
+
 # find the login form elements, fill in the form, and then submit it
-login_form_email = get_when_visible(browser, (By.ID, 'login-f_email'))
-login_form_password = get_when_visible(browser, (By.ID, 'login-f_password'))
+login_form_email = get_element((By.ID, 'login-f_email'))
+login_form_password = get_element((By.ID, 'login-f_password'))
 login_form_email.send_keys(email)
 login_form_password.send_keys(password)
 login_form_password.submit()
 
 # navigate to the interaction list
-get_when_visible(browser, (By.LINK_TEXT, "Pending & History")).click()
-get_when_visible(browser, (By.LINK_TEXT, "Show more >>")).click()
+get_element((By.LINK_TEXT, "Pending & History")).click()
+get_element((By.LINK_TEXT, "Show more >>")).click()
 
 # find the element that's displayed when the list is expanding
 busy_view = browser.find_element_by_class_name("busyView")
@@ -115,11 +112,14 @@ while read_count < 100:
 
             # finds the interaction's type
             class_name = elem.get_attribute('class')
-            type_search = re.search('(.+)ContactWidget ?(.*)', class_name)
-            if type_search.group(1): data['type'] = type_search.group(1).encode('utf8')
-            else: data['type'] = class_name
+            type_search = re.search('(.+)ContactWidget( ?.*)', class_name)
+            if type_search.group(1):
+                data['type'] = type_search.group(1).encode('utf8')
+            else:
+                data['type'] = class_name
 
-            if type_search.group(2): data['subtype'] = type_search.group(2).encode('utf8')
+            if type_search.group(2):
+                data['subtype'] = type_search.group(2).encode('utf8')
 
             # adds the interaction's date to the 'data' dictionary
             if   data['type'] == 'Event':   data['date'] = get_date(elem, '.created')
@@ -155,7 +155,7 @@ while read_count < 100:
 # displays the data in json format
 print dumps(submit_list, indent=4)
 
-
+browser.close()
 # TODO: submit submit_list to neo4j somehow
 
 
