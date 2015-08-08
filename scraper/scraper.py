740adbb1a11b61c4e61d4de10bba64bd76972886
Arie van Luttikhuizen <aluttik@gmail.com>
2015-08-08 09:43:52 +0100

    Read popups with recursion

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,31 +2,14 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common import keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime, date
-import json
+from json import dumps
 import os
 import re
 
-def click_link(driver, text, timeout=60):
-    '''Waits for a hyperlink to be clickable, then clicks it.
-
-    Parameters:
-        driver (webdriver) - the webdriver that's being used
-        text (str) - the text of the link to click on
-        timeout (int) - seconds to wait before timeout (default 60)
-    Returns:
-        None
-    Raises:
-        TimeoutException - when no element is found before timeout
-    '''
-    wait = WebDriverWait(driver, timeout)
-    locator = (By.LINK_TEXT, text)
-    link = wait.until(EC.element_to_be_clickable(locator))
-    link.click()
-
-def get_when_visible(driver, locator, timeout=60):
+def get_when_visible(driver, locator, timeout=30):
     '''Waits until a web element is be visible, then returns it.
 
     Parameters:
@@ -38,9 +21,7 @@ def get_when_visible(driver, locator, timeout=60):
     Raises:
         TimeoutException - when no element is found before timeout
     '''
-    wait = WebDriverWait(driver, timeout)
-    element = wait.until(EC.visibility_of_element_located(locator))
-    return element
+    return wait(driver, timeout).until(EC.visibility_of_element_located(locator))
 
 def get_date(top_elem, css_selector):
     '''Finds the date from an HTML element
@@ -63,45 +44,39 @@ def get_date(top_elem, css_selector):
         # if it does, return today
         return date.today().isoformat();
 
-def get_other_contacts(interaction_element, popup_link):
+def get_contacts(top_elem):
     '''Collects all of the contacts found in a "other contacts" popup
 
     Parameters:
-        interaction_element (webelement) - the interaction that's being processed
-        popup_link (webelement) - a link to an "other contacts" popup
+        top_elem (webelement) - the parent of the contact elements
     Returns:
         a dictionary of all contacts found in the popup
     '''
-    others = {}
-
-    # open and find the popup element
-    click_link(interaction_element, popup_link.text)
-    popup_element = browser.find_element_by_css_selector('body > div.nmbl-PopupWindow')
-
+    contact_list = {}
     # find the contacts who have ids
-    for link in popup_element.find_elements_by_css_selector('div.content div a.contact'):
-        url = link.get_attribute('href').encode('utf8')
-        link_search = re.search('^https?://.+?\.nimble.com(.*)$', url)
-        others[link.text.encode('utf8')] = link_search.group(1)
-
+    for link in top_elem.find_elements_by_tag_name('a'):
+        if link.get_attribute('class') == 'contact':
+            contact_list[link.text.encode('utf8')] = link.get_attribute('href').encode('utf8')
+        # if this is an "X others" link, add contacts from the popup it opens
+        elif link.get_attribute('class') == 'gwt-Anchor':
+            link.click()
+            popup_elem = browser.find_element_by_css_selector('body > div.nmbl-PopupWindow')
+            contact_list.update(get_contacts(popup_elem))
+            top_elem.find_element_by_css_selector('span.last').click()
     # find the contacts who don't have ids
-    for span in popup_element.find_elements_by_css_selector('div.content div span.notMatched'):
-        others[span.text.encode('utf8')] = 'notMatched'
+    for span in top_elem.find_elements_by_css_selector('span.notMatched'):
+        contact_list[span.text.encode('utf8')] = 'notMatched'
+    return contact_list
 
-    # close the popup element
-    interaction_element.find_element_by_css_selector('span.last').click()
-
-    return others
+# get information from environment variables
+email = os.environ['NIMBLE_EMAIL']
+password = os.environ['NIMBLE_PASSWORD']
+path_to_chromedriver = os.environ['PATH_TO_CHROMEDRIVER']
 
 # open a new chrome web driver and go to nimble.com
-path_to_chromedriver = os.environ['PATH_TO_CHROMEDRIVER']
 browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 url = "https://nginx.nimble.com/#app/contacts/view?id=5581fe938e08ab59fe6dd915"
 browser.get(url)
-
-# get login information from config variables
-email = os.environ['NIMBLE_EMAIL']
-password = os.environ['NIMBLE_PASSWORD']
 
 # find the login form elements, fill in the form, and then submit it
 login_form_email = get_when_visible(browser, (By.ID, 'login-f_email'))
@@ -111,45 +86,40 @@ login_form_password.send_keys(password)
 login_form_password.submit()
 
 # navigate to the interaction list
-click_link(browser, "Pending & History")
-click_link(browser, "Show more >>")
+get_when_visible(browser, (By.LINK_TEXT, "Pending & History")).click()
+get_when_visible(browser, (By.LINK_TEXT, "Show more >>")).click()
 
 # find the element that's displayed when the list is expanding
-busy_view = browser.find_element_by_class_name("busyView");
+busy_view = browser.find_element_by_class_name("busyView")
 
 submit_list = []
-# TODO: find most recent interaction
 
+# TODO: find most recent interaction
 table = browser.find_element_by_class_name('ContactEntitiesTable')
 
 # TODO: break when freshly loaded interactions are too old
 # TODO: loop until the last interaction read is already in database
 read_count = 0
 while read_count < 100:
-
     # when the expandable interaction list isn't expanding
     if busy_view.is_displayed() is False:
-
         # scroll to the bottom of the page to expand it
         browser.execute_script("scroll(0, document.body.scrollHeight)")
-
-        # read new interactions that have shown up as a result of scrolling
+        # read new interactions that have showed up as a result of scrolling
         currently_open = len(table.find_elements_by_tag_name('tr'))
         while read_count < currently_open:
-
             # focus on the next unread element in the interaction list
             read_count = read_count + 1
             elem = table.find_element_by_xpath('.//tr[%d]/td/div' % read_count)
-
             data = {}
-
 
             # finds the interaction's type
             class_name = elem.get_attribute('class')
             type_search = re.search('(.+)ContactWidget ?(.*)', class_name)
             if type_search.group(1): data['type'] = type_search.group(1).encode('utf8')
-            if type_search.group(2): data['subtype'] = type_search.group(2).encode('utf8')
+            else: data['type'] = class_name
 
+            if type_search.group(2): data['subtype'] = type_search.group(2).encode('utf8')
 
             # adds the interaction's date to the 'data' dictionary
             if   data['type'] == 'Event':   data['date'] = get_date(elem, '.created')
@@ -161,47 +131,29 @@ while read_count < 100:
                     f.write(class_name + ':\t')
                     f.write(elem.get_attribute('innerHTML') + '\n')
 
-
             # adds the interaction's subject to the 'data' dictionary
             try: data['subject'] = elem.find_element_by_css_selector('.subject').text.encode('utf8')
             except NoSuchElementException: data['subject'] = '(no subject)'
 
-
-            # adds the interaction's subject to the 'data' dictionary
-            try: data['sender'] = elem.find_element_by_css_selector('.details div.gwt-HTML *').text.encode('utf8')
-            except NoSuchElementException: pass
-
+            # adds the interaction's sender to the 'data' dictionary (if relevant)
+            try:
+                sender_elem = elem.find_element_by_css_selector('.details div.gwt-HTML *')
+                name = sender_elem.text
+                url = sender_elem.get_attribute('href') if sender_elem.tag_name is 'a' else 'notMatched'
+                data['sender'] = { name.encode('utf8') : url.encode('utf8') }
+            except NoSuchElementException:
+                pass
 
             # adds the contacts who were involved in the interaction
-            contacts = {}
-            contacts_element = elem.find_element_by_css_selector('div.ExpandedMoreContactsListWidget')
-
-            # first find all contacts who have associated urls
-            for link in contacts_element.find_elements_by_tag_name('a'):
-
-                # if the link is a contact, add it to the contacts dict
-                if link.get_attribute('class') == 'contact':
-                    url = link.get_attribute('href').encode('utf8')
-                    link_search = re.search('^https?://.+?\.nimble.com(.*)$', url)
-                    contacts[link.text.encode('utf8')] = link_search.group(1)
-
-                # if there is an "X others" link, grab contacts from that too
-                elif link.get_attribute('class') == 'gwt-Anchor':
-                    contacts.update(get_other_contacts(elem, link))
-
-            # then find all contacts who don't have associated urls
-            for span in contacts_element.find_elements_by_css_selector('span.notMatched'):
-                contacts[span.text.encode('utf8')] = 'notMatched'
-
+            contacts_elem = elem.find_element_by_css_selector('.ExpandedMoreContactsListWidget')
+            contacts = get_contacts(contacts_elem)
             if contacts: data['contacts'] = contacts
-
 
             # append this dictionary to the neo4j submit list
             submit_list.append(data)
 
-
 # displays the data in json format
-print json.dumps(submit_list, indent=4)
+print dumps(submit_list, indent=4)
 
 
 # TODO: submit submit_list to neo4j somehow
